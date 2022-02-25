fc62cd2852b58a7b3cbc4225128ad284958e003e
XOA <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>
2022-02-25 19:14:15 +0500

    update

--- src/components/feedback/Feedback.tsx
+++ src/components/feedback/Feedback.tsx
@@ -53,11 +53,7 @@ export const Feedback: React.FunctionComponent = () => {
             <button type="submit">Отправить</button>
           </div>
           <div className={styles.wrapper_container_info_maps}>
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d75060.86647034707!2d-118.30866158188985!3d33.754128042021485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c75ddc27da13%3A0xe22fdf6f254608f4!2z0JvQvtGBLdCQ0L3QtNC20LXQu9C10YEsINCa0LDQu9C40YTQvtGA0L3QuNGPLCDQodCo0JA!5e0!3m2!1sru!2sru!4v1645627779573!5m2!1sru!2sru"
-              allowfullscreen=""
-              loading="lazy"
-            ></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d75060.86647034707!2d-118.30866158188985!3d33.754128042021485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c75ddc27da13%3A0xe22fdf6f254608f4!2z0JvQvtGBLdCQ0L3QtNC20LXQu9C10YEsINCa0LDQu9C40YTQvtGA0L3QuNGPLCDQodCo0JA!5e0!3m2!1sru!2sru!4v1645627779573!5m2!1sru!2sru"></iframe>
           </div>
         </div>
       </div>
